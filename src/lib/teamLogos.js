dd1429ccfe4fe423be9d8e311fbf1d4a99381ab6
Nathan Micheas <nathanmicheas0@gmail.com>
2026-04-17 03:01:35 +0000

    File changes

--- src/lib/teamLogos.js
+++ src/lib/teamLogos.js
@@ -1,19 +1,17 @@
-// NBA team logos using official CDN
-// Format: https://cdn.nba.com/logos/nba/{teamId}/global/L/logo.svg
-
-const TEAM_IDS = {
-  ATL: 1610612737, BOS: 1610612738, BKN: 1610612751, CHA: 1610612766,
-  CHI: 1610612741, CLE: 1610612739, DAL: 1610612742, DEN: 1610612743,
-  DET: 1610612765, GSW: 1610612744, HOU: 1610612745, IND: 1610612754,
-  LAC: 1610612746, LAL: 1610612747, MEM: 1610612763, MIA: 1610612748,
-  MIL: 1610612749, MIN: 1610612750, NOP: 1610612740, NYK: 1610612752,
-  OKC: 1610612760, ORL: 1610612753, PHI: 1610612755, PHX: 1610612756,
-  POR: 1610612757, SAC: 1610612758, SAS: 1610612759, TOR: 1610612761,
-  UTA: 1610612762, WAS: 1610612764,
+// NBA team logos using ESPN CDN
+const ESPN_IDS = {
+  ATL: 'atl', BOS: 'bos', BKN: 'bkn', CHA: 'cha',
+  CHI: 'chi', CLE: 'cle', DAL: 'dal', DEN: 'den',
+  DET: 'det', GSW: 'gs',  HOU: 'hou', IND: 'ind',
+  LAC: 'lac', LAL: 'lal', MEM: 'mem', MIA: 'mia',
+  MIL: 'mil', MIN: 'min', NOP: 'no',  NYK: 'ny',
+  OKC: 'okc', ORL: 'orl', PHI: 'phi', PHX: 'phx',
+  POR: 'por', SAC: 'sac', SAS: 'sa',  TOR: 'tor',
+  UTA: 'utah', WAS: 'wsh',
 };
 
 export function getTeamLogoUrl(teamAbbr) {
-  const id = TEAM_IDS[teamAbbr?.toUpperCase()];
-  if (!id) return null;
-  return `https://cdn.nba.com/logos/nba/${id}/global/L/logo.svg`;
+  const slug = ESPN_IDS[teamAbbr?.toUpperCase()];
+  if (!slug) return null;
+  return `https://a.espncdn.com/i/teamlogos/nba/500/${slug}.png`;
 }
